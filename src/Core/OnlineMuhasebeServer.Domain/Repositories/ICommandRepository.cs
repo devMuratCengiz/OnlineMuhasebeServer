175e4a6912a73307b0a90dee0fbc33d1d821c03c
Murat Cengiz <devmuratcengiz@outlook.com>
2025-04-23 20:20:26 +0300

    son

--- src/Core/OnlineMuhasebeServer.Domain/Repositories/ICommandRepository.cs
+++ src/Core/OnlineMuhasebeServer.Domain/Repositories/ICommandRepository.cs
@@ -1,7 +1,16 @@
-﻿namespace OnlineMuhasebeServer.Domain.Repositories
+﻿using Microsoft.EntityFrameworkCore;
+using OnlineMuhasebeServer.Domain.Abstractions;
+
+namespace OnlineMuhasebeServer.Domain.Repositories
 {
-    public interface ICommandRepository
+    public interface ICommandRepository<T> : IRepository<T> where T :Entity
     {
-        
+        Task AddAsync(T entity);
+        Task AddRangeAsync(IEnumerable<T> entities);
+        void Update(T entity);
+        void UpdateRange(IEnumerable<T> entities);
+        Task RemoveById(string id);
+        void Remove(T entity);
+        void RemoveRange(IEnumerable<T> entities);
     }
 }
